d429512373735e252cb1bb03a8a6e39361a1580d
chin valery <chinvalery@gmail.com>
2018-09-10 15:25:56 +0200

    carrousel de la mort qui tue

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,9 @@ import './App.css'
 import Toolbar from './components/Toolbar/Toolbar.js'
 import SideDrawer from './components/SideDrawer/SideDrawer.js'
 import Backdrop from './components/Backdrop/Backdrop.js'
+import Carousel from "./components/Carousel/Carousel.js";
+import "./components/Carousel/Carousel.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class App extends Component {
   
@@ -36,6 +39,11 @@ class App extends Component {
       <main className="body">
         <p>This is the main page</p>
       </main>
+      <div className="Carrousel-size">
+      <Carousel />
+      </div>
+  
+  
     </div>
     );
   }
