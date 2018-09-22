0de211fdb376a8c6a94d0b1d5f9f982b17611e21
Ludivine <ludivine.pellissier@gmail.com>
2018-09-22 19:13:15 +0200

    create div content with presentation + CSS - not finished, fixes needed

--- my-app/src/containers/Homepage.js
+++ my-app/src/containers/Homepage.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
-import '../App.css'
+import './style/Homepage.css'
 import '../components/Carousel/Carousel.css'
 import Navbar from './Navbar.js'
 import Carousel from '../components/Carousel/Carousel.js'
+import presentationAsso from '../datas/presentationAsso.json'
 
 class Homepage extends Component {
+  state = {
+    presentationAsso : '',
+  }
+
+  componentDidMount() {
+    this.setState({presentationAsso: presentationAsso})
+  }
+
   render() {
     return (
-      <div>
+      <div className='homepage'>
         <Navbar />
-        <main className='body'>
-          Homepage
-      </main>
-        <div className='carousel-size'>
+        <div className='homepage-carousel-size'>
           <Carousel />
+        </div>
+        <div className='homepage-presentation-container'>
+          <div className='homepage-presentation-background'>
+            <div className='homepage-presentation-image'></div>
+          </div>
+          <div className='homepage-presentation-content'>
+            <div className='homepage-presentation-text'>
+              <main>
+                <p>{presentationAsso.part1}</p>
+                <p>{presentationAsso.part2}</p>
+              </main>
+            </div>
+          </div>
         </div>
       </div>
     )
