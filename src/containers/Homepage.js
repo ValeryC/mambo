570d0c37dcb30935c67db6291a6aee9459569c24
Ludivine <ludivine.pellissier@gmail.com>
2018-09-30 03:54:49 +0200

    add presentation asso + set fixed  background on scroll + css

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -3,18 +3,41 @@ import './style/Homepage.css'
 import '../components/Carousel/Carousel.css'
 import Navbar from './Navbar.js'
 import Carousel from '../components/Carousel/Carousel.js'
-import presentationAsso from '../datas/presentationAsso.json'
+import presentationAssoGlobal from '../datas/presentationAsso.json'
 
 class Homepage extends Component {
   state = {
-    presentationAsso : '',
+    presentationAsso: '',
   }
 
   componentDidMount() {
-    this.setState({presentationAsso: presentationAsso})
+    this.setState({ presentationAsso: presentationAssoGlobal }, () => {
+      const presentationPart1 = document.getElementById('presentationPart1')
+      presentationPart1.innerHTML = this.state.presentationAsso.part1
+      const presentationPart2 = document.getElementById('presentationPart2')
+      presentationPart2.innerHTML = this.state.presentationAsso.part2
+    })
+
+    window.onscroll = () => { stickBackground() }
+
+    const backgroundPart = document.getElementById('background-part')
+    const contentPart = document.getElementById('content-part')
+
+    const stickBackground = () => {
+      const add = () => {
+        backgroundPart.classList.add('homepage-presentation-background-fixed')
+        contentPart.classList.add('homepage-presentation-content-scroll')
+      }
+      const remove = () => {
+        backgroundPart.classList.remove('homepage-presentation-background-fixed')
+        contentPart.classList.remove('homepage-presentation-content-scroll')
+      }
+      window.pageYOffset >= window.innerHeight ? add() : remove()
+    }
   }
 
   render() {
+
     return (
       <div className='homepage'>
         <Navbar />
@@ -22,14 +45,16 @@ class Homepage extends Component {
           <Carousel />
         </div>
         <div className='homepage-presentation-container'>
-          <div className='homepage-presentation-background'>
+          <div id='background-part' className='homepage-presentation-background'>
             <div className='homepage-presentation-image'></div>
           </div>
-          <div className='homepage-presentation-content'>
+          <div id='content-part' className='homepage-presentation-content'>
             <div className='homepage-presentation-text'>
               <main>
-                <p>{presentationAsso.part1}</p>
-                <p>{presentationAsso.part2}</p>
+                <h1>Mambo Salsa</h1>
+                <p id='presentationPart1'></p>
+                <iframe width="100%" height="540" src="https://www.youtube.com/embed/3iq1_xZ0vTo" frameborder="0" allowfullscreen></iframe>
+                <p id='presentationPart2'></p>
               </main>
             </div>
           </div>
